68cfe07514f53231e5c80db2f5aa538a314b70eb
Diazt Muhammad Firmansyah <diaztmuhammadfirmansyah@gmail.com>
2026-07-16 07:42:37 +0700

    chore: build project assets and update production manifest

---
+++ public/build/assets/login-ad1vXYID.js
@@ -0,0 +1,1 @@
+import{n as e}from"./wayfinder-Bsu-DpqY.js";var t=e=>({url:t.url(e),method:`post`});t.definition={methods:[`post`],url:`/login`},t.url=n=>t.definition.url+e(n),t.post=e=>({url:t.url(e),method:`post`});var n=e=>({action:t.url(e),method:`post`});n.post=e=>({action:t.url(e),method:`post`}),t.form=n,Object.assign(t,t);export{t};
